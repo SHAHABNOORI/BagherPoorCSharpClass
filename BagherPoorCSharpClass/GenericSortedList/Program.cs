eaaa61a1f40bebc3ac23f57e96db7d8fd09af1d6
Shahab-Pc <Shahab-Pc@DESKTOP-3048NIP>
2020-10-22 20:27:00 +0330

    Init Generic Sorted List

---
+++ BagherPoorCSharpClass/GenericSortedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> students = new SortedList<int, string>();
+            students.Add(4, "Shahab");
+            students.Add(2, "Mahdi");
+            students.Add(3, "Vahid");
+            students.Add(1, "Behnam");
+            students.Add(1, "Morad");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key} {student.Value}");
+            }
+        }
+    }
+}
